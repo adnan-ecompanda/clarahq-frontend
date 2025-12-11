23f84aba5c797729f5986b4b41081091660d3bac
adnan-ecompanda <daniweber3561@gmail.com>
2025-12-11 15:46:46 +0500

    api commit for meta

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WhatsAppWebhookController;
 
 Route::get('/whatsapp/webhook', [WhatsAppWebhookController::class, 'verify']);
